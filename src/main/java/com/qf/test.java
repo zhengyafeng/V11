b0650c1bb97cd33575edc590287beccf17724d3d
zhengyafeng <53290049+zhengyafeng@users.noreply.github.com>
2019-11-20 00:46:54 +0800

    Update test.java

--- src/main/java/com/qf/test.java
+++ src/main/java/com/qf/test.java
@@ -4,7 +4,7 @@ public class test {
 
     public static void main(String[] args) {
         System.out.println("bbbbbbbbbbbbb");
-        System.out.println("腿毛哥吃大便。。。吃一顿还不够");
+        System.out.println("腿毛哥吃大便。。。吃一顿还不够，吃两顿");
         System.out.println("aaaaaaaaaaaaaaa");
     }
 }
