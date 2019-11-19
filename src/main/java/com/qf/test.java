d0a821c456032d7c121ef8a47ed85edbf08e90a0
zhengyafeng <1078710424@qq.com>
2019-11-20 00:56:55 +0800

    五

--- src/main/java/com/qf/test.java
+++ src/main/java/com/qf/test.java
@@ -2,9 +2,9 @@ package com.qf;
 
 public class test {
 
-    public static void main(String[] args,String s,String s,String s,String sString sString sString sString sString sString s) {
+    public static void main(String[] args) {
         System.out.println("bbbbbbbbbbbbb");
-        System.out.println("腿毛哥吃大便。。。吃一顿还不够，吃两顿",sdfsdgsfgdfshsd);
+        System.out.println("腿毛哥吃大便。。。吃一顿还不够，吃两顿,吃一顿还不够，吃五顿");
         System.out.println("腿毛哥吃大便。。。吃一吨还不够,吃三吨");
         System.out.println("aaaaaaaaaaaaaaa");
         System.out.println("腿毛哥吃大便。。。吃一吨还不够,吃三吨");
