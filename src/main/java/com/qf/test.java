cbf587051c269a1e71c3de41bf9659489586f5c3
zhengyafeng <53290049+zhengyafeng@users.noreply.github.com>
2019-11-20 00:53:58 +0800

    Update test.java

--- src/main/java/com/qf/test.java
+++ src/main/java/com/qf/test.java
@@ -2,7 +2,7 @@ package com.qf;
 
 public class test {
 
-    public static void main(String[] args,String s,String s,String s) {
+    public static void main(String[] args,String s,String s,String s,String sString sString sString sString sString sString s) {
         System.out.println("bbbbbbbbbbbbb");
         System.out.println("腿毛哥吃大便。。。吃一顿还不够，吃两顿",sdfsdgsfgdfshsd);
         System.out.println("腿毛哥吃大便。。。吃一吨还不够,吃三吨");
