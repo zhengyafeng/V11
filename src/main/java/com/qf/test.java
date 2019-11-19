a2f4f920b1cd3d5f6b3de4081a32cb412e4ed9e2
zhengyafeng <53290049+zhengyafeng@users.noreply.github.com>
2019-11-20 00:40:52 +0800

    Update test.java

--- src/main/java/com/qf/test.java
+++ src/main/java/com/qf/test.java
@@ -3,6 +3,6 @@ package com.qf;
 public class test {
 
     public static void main(String[] args) {
-        System.out.println("bbbbbbbbbbbbb");
+        System.out.println("aaaaaaaaaaaaaaa");
     }
 }
